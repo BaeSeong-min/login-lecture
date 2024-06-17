a071683a9adf69cb418aa2f4d5545c811939285c
baeseongmin000 <29343574-baeseongmin000@users.noreply.replit.com>
2024-06-17 10:44:24 +0000

    프런트 단 javascript 파일 만들기

--- app/app.js
+++ app/app.js
@@ -10,6 +10,7 @@ const home = require("./src/routes/home");
 // 앱 세팅
 app.set("views", "./src/views");
 app.set("view engine", "ejs"); // view엔진인 ejs, ejs은 html과 굉장히 비슷
+app.use(express.static(`${__dirname}/src/public`)) // login.js와 login.ejs 연결
 
 app.use("/", home); // use -> 미들 웨어를 등록해주는 메서드.
 
